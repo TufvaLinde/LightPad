48cbba9fb460dcfbc2275949bff302ab7d5838ba
Tuva Ekedal <tuva.ekedal@2xper.se>
2020-02-04 13:26:08 +0100

    Startup interface etc

--- LightPad/ContentView.swift
+++ LightPad/ContentView.swift
@@ -7,30 +7,49 @@
 //
 
 import SwiftUI
+import AVKit
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.gray
-            .edgesIgnoringSafeArea(.all)
+            Color(red:0.72, green:0.72, blue:0.94, opacity:1.0)
+                .edgesIgnoringSafeArea(.all)
             VStack {
                 Image("Title")
-                Text("Hello LightPad user!")
-                    .foregroundColor(.red)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 600.0,height: 600.0)
                 Spacer()
-                Button(action: {
-                    //vad som händer när man klickar på knappen
-                }) {
+                NavigationLink(destination: Text("")) {
                     Text("Lit me up!")
-                        .foregroundColor(.yellow)
-                }
-            }.padding()
+                        .font(.largeTitle)
+                        .fontWeight(.thin)
+                        .foregroundColor(Color(red: 1.0, green: 0.5, blue: 0.0, opacity: 1.0))
+                }.buttonStyle(PlainButtonStyle())
+                Spacer()
+            }
+        }.onAppear(perform: playVideo)
+    }
+
+    func playVideo() {
+        guard let path = Bundle.main.path(forResource: "video", ofType:"MOV") else {
+            debugPrint("video.MOV not found")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) {
+            player.play()
         }
     }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
